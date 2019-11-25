02076e235ef3b0878d2498c613f965e4b30ec1ec
Alfredo Diaz <mail4alfredo@gmail.com>
2019-11-25 16:56:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,38 @@
-def using_include(array, element)
+require 'pry'
 
+def using_include(array, element)
+  binding.pry
+  #iterators 
+  #array => type of data structure
+  #string => data structure
+  #hash 
+  
+  #air = [a, b, c, d, 1, 2, 3, 4, cat, dog, bird]
+  #index
+  #[0, 1, 2...]
+  #air[3] => d
+  
+  
+  
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+ array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+ array.size
 end
